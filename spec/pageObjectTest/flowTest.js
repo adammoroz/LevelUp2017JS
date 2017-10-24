3d60dd11d979a9189a9aaacacf72bca223e84874
CORP\AMOROZ <Adam.moroz@capgemini.com>
2017-10-24 16:17:31 +0200

    ver2

--- spec/pageObjectTest/flowTest.js
+++ spec/pageObjectTest/flowTest.js
@@ -6,17 +6,15 @@ var ContactUs = require(pageObjectDir+"/contactUsPage.js")
 var homePage = new HomePage();
 var contactUs = new ContactUs();
 
-describe('Protractor Demo App',  function  () {
-    it('should have a title',  function  () {
+describe('ContuctUs tests',  function  () {
+    it('open page',  function  () {
         browser.waitForAngularEnabled(false);
         browser.get(homePage.URL);
 
-        expect(homePage.cartLabel.getText())
-            .toEqual("Cart (empty)");
     });
     it('should click contactUs link', function () {
         homePage.contactUsLink.click();
 
     });
-
+    
 });
